8129b18a88142fc746b6e2093c4dff8b67947b64
Brian <clockworkai@outlook.com>
2024-04-23 17:37:52 +0100

    cleanup

--- modelling/modelling.py
+++ modelling/modelling.py
@@ -1,9 +1,7 @@
 from model.randomforest import RandomForest
 from modelling.chainer import Chainer
 
-
 def model_predict(data, chainer:Chainer=None):
-    results = []
     print("RandomForest")
     model = RandomForest("RandomForest", data.get_embeddings(), data.get_type())
     model.train(data)
